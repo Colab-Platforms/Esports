619a3025781fddd1dc74988060e75c97eafcc967
tech975 <tech@colabplatforms.com>
2026-03-20 11:52:03 +0530

    fixed the freefire validation

--- server/routes/freeFireRegistration.js
+++ server/routes/freeFireRegistration.js
@@ -1,4 +1,4 @@
-const express = require('express');
+ const express = require('express');
 const { body, validationResult, query } = require('express-validator');
 const TournamentRegistration = require('../models/TournamentRegistration');
 const WhatsAppMessage = require('../models/WhatsAppMessage');
